a2eef13e21226160b914652d6a492ea3b214d868
SPESNA862 <fabriziobussalleu@gmail.com>
2025-10-23 00:24:58 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/LawAI/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
