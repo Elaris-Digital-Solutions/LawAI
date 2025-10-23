2153dd5ca81f019c91d078ad2acda7ceff5b6d23
Elkfle <corloscalfer@gmail.com>
2025-10-23 00:08:54 -0500

    Revert "Merge branch 'main' of https://github.com/Elaris-Digital-Solutions/LawAI"
    This reverts commit 352a334ddae83c0c03234304fb1a6762e5484209, reversing
    changes made to c43ac5b466b860a4d4eac3a27755f0181b8e950a.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === 'build' ? '/LawAI/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
   }
-});
+}));
